138bbca92ef5a368756b2db3f35982c1dbf7bfe8
Tiancheng Qin <tianchengq@gmail.com>
2017-11-07 16:14:11 -0500

    minor changes for lstm

--- parse.py
+++ parse.py
@@ -1,11 +1,23 @@
 import tensorflow as tf
 import numpy as np
 import music21 as m21
+import collections
 
 test_midi = 'http://kern.ccarh.org/cgi-bin/ksdata?l=cc/bach/cello&file=bwv1007-01.krn&f=xml'
 
+def build_dataset(notes):
+    # notes is an array of (note, duration) elements
+    #   - note is a string (ex. 'A4')
+    #   - duration is a float representing number of quarter notes (ex. 0.25)
+    count = collections.Counter(notes).most_common()
+    dictionary = {}
+    for note, _ in count:
+        dictionary[note] = len(dictionary)
+    reverse_dictionary = dict(zip(dictionary.values(), dictionary.keys()))
+    return dictionary, reverse_dictionary
+
 def parse_music(file_name, num_voices=1, show=False):
-    # returns an array of [note, duration] elements
+    # returns an array of (note, duration) elements
     #   - note is a string (ex. 'A4')
     #   - duration is a float representing number of quarter notes (ex. 0.25)
 
@@ -25,11 +37,26 @@ def parse_music(file_name, num_voices=1, show=False):
         for measure in measures:
             for note in measure.notes:
                 try:
-                    data.append([note.nameWithOctave, note.duration.quarterLength]) # type: [string, float]
+                    data.append((note.nameWithOctave, note.duration.quarterLength)) # type: [string, float]
                 except:
                     pass
         return data
         
     return
 
-# print(parse_music(test_midi, show=False))
+vec_to_num, num_to_vec = build_dataset(parse_music(test_midi))
+print(vec_to_num)
+print(num_to_vec)
+
+
+
+
+
+
+
+
+
+
+
+
+
